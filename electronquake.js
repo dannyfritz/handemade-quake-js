574d197175ff38c4ef232d8967c6ae55cec5ce6a
Danny Fritz <dannyfritz@gmail.com>
2016-02-07 13:53:50 -0700

    :sparkles: Create an 800x600 window

--- electronquake.js
+++ electronquake.js
@@ -1,4 +1,21 @@
-const electron = require("electron");
+"use strict"
+const electron = require("electron")
+const BrowserWindow = electron.BrowserWindow
 
-const app = electron.app;
-app.quit();
+const app = electron.app
+let mainWindow = null
+
+app.on("ready", () => {
+  mainWindow = new BrowserWindow({
+    title: "Module 2.1",
+    resizable: false,
+  })
+  mainWindow.setMenu(null)
+  mainWindow.setContentSize(800, 600)
+  mainWindow.loadURL('file://' + __dirname + '/index.html')
+  mainWindow.webContents.openDevTools({detach: true})
+  mainWindow.on("closed", () => {
+    mainWindow = null
+    app.quit()
+  })
+})
