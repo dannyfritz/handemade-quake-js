6d4611f7149221011d205cd3a43051fe6f3c27f7
Danny Fritz <dannyfritz@gmail.com>
2016-02-07 15:05:26 -0700

    :clock1: first timers in gameloop

--- main.js
+++ main.js
@@ -5,3 +5,16 @@ const ipcRenderer = require('electron').ipcRenderer
 document.addEventListener('keydown', () => {
   ipcRenderer.send('quit')
 })
+
+let tick = performance.now()
+
+const main = (tock) => {
+  requestAnimationFrame(main)
+  const secondsGoneBy = (tock - tick) / 1000
+  console.log(secondsGoneBy)
+  //update()
+  //render()
+  tick = performance.now()
+}
+
+main()
